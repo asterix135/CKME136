5b106c238275b8c358fcd5956244784cea9badbe
asterix135 <chgraham@gmail.com>
2016-02-05 17:31:15 -0500

    image size updated

--- Python_code/exploratory/images/image_sizes.py
+++ Python_code/exploratory/images/image_sizes.py
@@ -4,23 +4,61 @@ http://stackoverflow.com/questions/1507084/how-to-check-dimensions-of-all-images
 http://stackoverflow.com/questions/7460218/get-image-size-without-downloading-it-in-python
 """
 
-import urllib.request as urllib
+# import urllib.request as urllib
 from PIL import Image
 from io import BytesIO
 import requests
-from Python_code import sql_connect as sql
+from Python_code import sql_connect as mysql
 
 
-href = 'http://pbs.twimg.com/media/CZg3tZyUcAACpMg.jpg'
+TESTING = True
+
+def is_valid_image(url):
+    """
+    Returns boolean as to whether image url is still valid
+    :param url: string
+    :return: boolean
+    """
+    try:
+        urllib.urlopen(url)
+        valid_url = True
+    except:
+        valid_url = False
+    return valid_url
 
 
-
-def get_size(url):
+def get_size(tweet, fail_list):
     # image = Image.open(filepath)
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    width, height = img.size
-    print(width, height)
+    try:
+        response = requests.get(tweet['image_url'])
+        img = Image.open(BytesIO(response.content))
+        width, height = img.size
+        print(width, height)
+    except:
+        fail_list.append(tweet['tweet_id'])
 
 
-get_size(href)
+def load_tweet_list():
+    # open database connection
+    connection = mysql.connect()
+
+    # pull record id, username and image url from all downloaded tweets
+    with connection.cursor() as cursor:
+        sql = "SELECT tweet_id, image_url " \
+              "FROM Original_tweets"
+        if TESTING:
+            sql = sql + ' LIMIT 50'
+        cursor.execute(sql)
+        url_list = cursor.fetchall()
+    connection.close()
+    return url_list
+
+
+def calculate_image_stats():
+    fail_list = []
+    tweet_list = load_tweet_list()
+
+
+
+if __name__ == '__main__':
+    pass
