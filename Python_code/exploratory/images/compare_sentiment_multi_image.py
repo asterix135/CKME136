bb9e542cef14e61b7682bf881399234c528888a1
asterix135 <chgraham@gmail.com>
2016-02-17 17:10:21 -0500

    updates

--- Python_code/exploratory/images/compare_sentiment_multi_image.py
+++ Python_code/exploratory/images/compare_sentiment_multi_image.py
@@ -2,3 +2,25 @@
 Tests to see how sentiment ratings compare on images that appear
 numerous times
 """
+
+import os
+from Python_code import sql_connect as mysql
+from PIL import Image
+import math
+import pandas as pd
+
+def get_repeats():
+    connection = mysql.connect()
+    with connection.cursor() as cursor:
+        sql = 'SELECT DISTINCT primary_tweet, ' \
+              'FROM Duplicate_images'
+        cursor.execute(sql)
+        repeat_df = pd.DataFrame(cursor.fetchall())
+    connection.close()
+    return repeat_df
+
+
+def compare_sentiments(repeat_df):
+
+    for i in range(len(repeat_df)):
+        pass
