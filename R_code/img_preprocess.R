26a28cb4d223a391a1e9d5bd986473c5d0c24d05
asterix135 <chgraham@gmail.com>
2016-03-02 22:02:19 -0500

    R code for NN started

---
+++ R_code/img_preprocess.R
@@ -0,0 +1,55 @@
+curwd <- getwd()
+setwd('..')
+source('Python_code/sql_vals.py')
+library(caret)
+library(RMySQL)
+library(jpeg)
+
+# potential packages: 
+# h20 deep learning
+# deepnet - in caret package
+
+IMAGE_DIR = '/Volumes/NeuralNet/images/'
+
+
+img_to_flat_matrix <- function(filename) {
+    img <- readJPEG(filename)
+    if (length(dim(img)) == 2){
+        return(NULL)
+    }
+    return(img)
+}
+
+
+get_data <- function(class_count=1000, image_path=IMAGE_DIR) {
+    con <- dbConnect(MySQL(),
+                     user=user,
+                     password=password,
+                     dbname=db,
+                     host=host,
+                     port=port)
+    sql <- paste('(SELECT tweet_id, tweet_sentiment FROM Original_tweets',
+                 'WHERE unclear_sentiment = 0 AND tweet_sentiment = 0 ',
+                 'LIMIT ', class_count, ') ',
+                 'UNION ALL ',
+                 '(SELECT tweet_id, tweet_sentiment FROM Original_tweets ',
+                 'WHERE unclear_sentiment = 0 AND tweet_sentiment = 1 ',
+                 'LIMIT ', class_count, ') ',
+                 'UNION ALL ',
+                 '(SELECT tweet_id, tweet_sentiment FROM Original_tweets ',
+                 'WHERE unclear_sentiment = 0 AND tweet_sentiment = -1 ',
+                 'LIMIT ', class_count, ')')
+    rs <- dbSendQuery(con, sql)
+    data <- fetch(rs, n=-1)
+    dbDisconnect(con)
+    
+    for (i in 1:length(data)) {
+        image <- paste(image_path, data[i,'tweet_id'], '.jpg', sep = '')
+        flat <- img_to_flat_matrix(image)
+        if (flat == NULL){
+            next
+        }
+    }
+}
+
+setwd(curwd)
