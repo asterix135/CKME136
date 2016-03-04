94a2171b0d053c9e971f3baeeef67475a24a5d8c
asterix135 <chgraham@gmail.com>
2016-03-04 08:22:18 -0500

    R code for NN started

--- R_code/img_preprocess.R
+++ R_code/img_preprocess.R
@@ -3,15 +3,17 @@ setwd('..')
 source('Python_code/sql_vals.py')
 library(RMySQL)
 library(jpeg)
+library(caret)
 
 # potential packages: 
 # h20 deep learning
 # deepnet - in caret package
 
 IMAGE_DIR = '/Volumes/NeuralNet/images/'
+SIZE = c(400, 400)
 
 
-img_to_flat_matrix <- function(filename) {
+img_to_flat_matrix <- function(filename, size) {
     img <- readJPEG(filename)
     if (length(dim(img)) == 2){
         return(NULL)
@@ -21,7 +23,7 @@ img_to_flat_matrix <- function(filename) {
 }
 
 
-get_data <- function(class_count=1000, image_path=IMAGE_DIR) {
+get_data <- function(class_count=500, image_path=IMAGE_DIR, size=SIZE) {
     con <- dbConnect(MySQL(),
                      user=user,
                      password=password,
@@ -43,13 +45,27 @@ get_data <- function(class_count=1000, image_path=IMAGE_DIR) {
     data <- fetch(rs, n=-1)
     dbDisconnect(con)
     
+    yval <- rep(c(0,1,-1), each=class_count)
+    yval_keep <- rep(TRUE, class_count*3)
+    
+    img_matrix = matrix(nrow=0, ncol=480000)
+    
     for (i in 1:length(data)) {
         image <- paste(image_path, data[i,'tweet_id'], '.jpg', sep = '')
-        flat <- img_to_flat_matrix(image)
-        if (flat == NULL){
+        flat <- img_to_flat_matrix(image, size)
+        if (is.null(flat)){
+            yval_keep[i] = FALSE
             next
         }
+        img_matrix <- rbind(img_matrix, flat)
     }
+    yval <- yval[yval_keep]
+    return (list(img_matrix, yval))
 }
+
+x_and_y <- get_data(); x_vals <- x_and_y[1]; y_vals <- x_and_y[2]
+
+
+
 
 setwd(curwd)
