98ba458ad54af4591ebcec521810efa1a0dcccfb
asterix135 <asterix135@users.noreply.github.com>
2016-03-11 17:22:05 -0500

    Update mlp.py

--- Python_code/classifiers/neural_net/mlp.py
+++ Python_code/classifiers/neural_net/mlp.py
@@ -4,14 +4,19 @@ http://deeplearning.net/tutorial/logreg.html
 http://deeplearning.net/tutorial/mlp.html
 http://deeplearning.net/tutorial/lenet.html
 """
+__docformat__ = 'reconstructedtext en'
+
 from Python_code.classifiers.preprocessing import img_preprocess as prep
 from sklearn.cross_validation import train_test_split
+import six.moves.cPickle as pickle
 import os
 import sys
+import timeit
 import numpy as np
 import theano
 import theano.tensor as T
 
+prep.SIZE = (100,100)
 
 class LogisticRegression:
     """Multi-class Logistic Regression Class
@@ -132,8 +137,7 @@ class LogisticRegression:
             raise NotImplementedError()
 
 
-def load_data(size=(100, 100), num_records=1000):
-    prep.SIZE = size
+def load_data(size=prep.SIZE, num_records=1750):
     data, labels = prep.get_data(num_records)
     train_x, test_x, train_y, test_y = train_test_split(
         data, labels, test_size=0.3, random_state=28022016
@@ -190,3 +194,193 @@ def sgd_optimization(learning_rate=0.13, n_epochs=1000, batch_size=600):
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0] // batch_size
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print('... building the model')
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+
+    # generate symbolic variables for input (x and y represent a minibatch)
+    x = T.matrix('x')  # data, presented as rasterized images
+    y = T.ivector('y')  # labels, presented as 1D vector of [int] labels
+
+    # construct the logistics regression class
+    # default to size used in loading images *3 (for rgb)
+    classifier = LogisticRegression(input=x,
+                                    n_in=prep.SIZE[0] * prep.SIZE[1] * 3,
+                                    n_out=3)
+
+    # the cost we minimize during the training is the negative log likelihood of
+    # the model in symbolic format
+    cost = classifier.negative_log_likelihood(y)
+
+    # compiling a Theano function that computes the mistakes that are made by
+    # the model on a minibatch
+    test_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+            y: test_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    validate_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    # compute the gradient of cost with respect to theta = (W,b)
+    g_W = T.grad(cost=cost, wrt=classifier.W)
+    g_b = T.grad(cost=cost, wrt=classifier.b)
+
+    # start-snippet-3
+    # specify how to update the parameters of the model as a list of
+    # (variable, update expression) pairs.
+    updates = [(classifier.W, classifier.W - learning_rate * g_W),
+               (classifier.b, classifier.b - learning_rate * g_b)]
+
+    # compiling a Theano function 'train_model' that returns the cost, but in
+    # the same time updates the parameter of the model based on the rules
+    # defined in 'updates'
+    train_model = theano.function(
+        inputs=[index],
+        outputs=cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    # end-snippet-3
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print('... training the model')
+    # early-stopping parameters
+    patience = 5000  # look at this many example regardless
+    patience_increase = 2  # wait this much longer when a new best is found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
+    validation_frequency = min(n_train_batches, patience //2)
+                                  # go through this many
+                                  # minibatches before checking the network
+                                  # on the validation set; in this case
+                                  # we check every epoch
+
+    best_validation_loss = np.inf
+    test_score = 0
+    start_time = timeit,default_timer()
+
+    done_looping = False
+    epoch = 0
+    while (epoch < n_epochs) and (not done_looping):
+        epoch += 1
+        for minibatch_index in range(n_train_batches):
+
+            minibatch_avg_cost = train_model(minibatch_index)
+            # iteration number
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            if (iter + 1) % validation_frequency == 0:
+                # compute zero-one loss on validation set
+                validation_losses = [validate_model(i)
+                                     for i in range(n_valid_batches)]
+                this_validation_loss = np.mean(validation_losses)
+
+                print(
+                    'epoch %i, minibatch %i/%i, validation error %f %%' %
+                    (
+                        epoch,
+                        minibatch_index + 1,
+                        n_train_batches,
+                        this_validation_loss * 100.
+                    )
+                )
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+                    # improve patience if loss improvement is good enough
+                    if this_validation_loss < best_validation_loss * \
+                       improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    best_validation_loss = this_validation_loss
+                    # test it on the test set
+
+                    test_losses = [test_model(i)
+                                   for i in range(n_test_batches)]
+                    test_score = np.mean(test_losses)
+
+                    print(
+                        (
+                            '   epoch %i, minibatch %i/%i, test error of'
+                            ' best model %f %%'
+                        ) %
+                        (
+                            epoch,
+                            minibatch_index + 1,
+                            n_train_batches,
+                            test_score * 100.
+                        )
+                    )
+
+                    # save teh best model
+                    with open('best_model.pkl', 'wb') as f:
+                        pickle.dump(classifier, f)
+
+            if patience <= iter:
+                done_looping = True
+                break
+    end_time = timeit.default_timer()
+    print(
+        (
+            'Optimization complete with best validation score of %f %%,'
+            'with test performance %f %%'
+        )
+        % (best_validation_loss * 100., test_score * 100.)
+    )
+    print('The code ran for %d epochs, with %f epochs/sec' % (
+        epoch, 1. * epoch / (end_time - start+time)))
+    print(('The code for file ' +
+           os.path.split(__file__)[1] +
+           ' ran for $.1fs' % ((end_time = start_time))), file=sys.stderr)
+
+
+def predict():
+    """
+    An example of how to load a trained model and use it to predict labels
+    """
+
+    # load the saved model
+    classifier = pickle.load(open('best_model.pkl'))
+
+    # compile a predictor function
+    predict_model = theano.function(
+        inputs=[classifier.input],
+        outputs=classifier.y_pred)
+
+    # We can test it on some examples from test test
+    datasets = load_data()
+    test_set_x, test_set_y = datasets[2]
+    test_set_x = test_set_x.get_value()
+
+    predicted_values = predict_model(test_set_x[:10])
+    print('Predicted values for the first 10 examples in the test set:')
+    print(predicted_values)
+
+if __name__ == '__main__':
+    sgd_optimization()
